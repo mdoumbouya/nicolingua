64b8d3ef651bc58c917ca676e12155e7d4a67c68
Moussa Doumbouya <doumbouya.moussa@gmail.com>
2021-01-02 17:10:11 +0000

    Added agent speech in fr, pu and su

--- scripts/webdemo/config_mac_book.py
+++ scripts/webdemo/config_mac_book.py
@@ -1,6 +1,3 @@
-DEBUG = True
-SEND_FILE_MAX_AGE_DEFAULT = 1
-
 WAV2VEC_CHECKPOINT_PATH = "/media/xtrem/data/experiments/nicolingua-0003-wa-wav2vec/wav2vec-training-exp-01/checkpoints/checkpoint_best.pt"
 # VAASR_CHECKPOINT_PATH = "/media/xtrem/code/git/nicolingua/notebooks/E400/results_400/checkpoints/VAASRCNN3PoolMaxAggMax/retrained-wav2vec_features-c_0_checkpoints/1000.pt"
 VAASR_CHECKPOINT_PATH = "/media/xtrem/code/git/nicolingua/notebooks/E401/results_401/checkpoints/VAASRCNN3PoolAvgAggMax/retrained-wav2vec_features-c_0_checkpoints/0800.pt"
@@ -9,4 +6,3 @@ WAV2VEC_CHECKPOINT_PATH = "/Users/moussadoumbouya/acoustic_models/nicolingua-000
 VAASR_CHECKPOINT_PATH = "/Users/moussadoumbouya/acoustic_models/VAASRCNN3PoolAvgAggMax_E401_wawav2vec-c_0_checkpoint_0800.pt"
 
 MAX_SEQUENCE_LENGTH = 300
-
